05f8bd03e36ef26810476c649577849f95e14892
Furkan <furkan.turkal@hotmail.com>
2020-05-28 23:08:24 +0300

    parser: let impl without expression system
    Signed-off-by: Furkan <furkan.turkal@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,283 @@
-use std::fmt;
+use std::{fmt, result, error};
+use std::io;
+use std::fmt::Error;
+use std::borrow::BorrowMut;
+
+const PROMPT: &str = ">> ";
 
 fn main() {
+	let mut input = String::new();
+
+	loop {
+		let s = match io::stdin().read_line(&mut input) {
+			Ok(_) => {
+				let mut l = Lexer::new(input.to_owned());
+
+				while let tok = l.next_token() {
+					if tok != Token::EOF {
+						println!("TOKEN: {}", tok);
+					}
+				}
+			},
+			Err(e) => {
+				println!("Something went wrong: {}", e);
+				break
+			},
+		};
+	}
+
 	println!("Hello, world!");
 }
+
+//=== AST BEGIN ===
+
+pub enum Node {
+	PROGRAM(Program),
+	STATEMENT(Statement),
+	EXPRESSION(Expression),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Literal {
+	INT(usize),
+	STRING(String),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Expression {
+	IDENT(String),
+	LITERAL(Literal)
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct LetStatement {
+	name: String,
+	value: Expression,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Statement {
+	INCOMPLETE,
+
+	//name, value
+	LET(LetStatement),
+
+	//value => x + y
+	EXP(Expression)
+}
+
+pub struct Program {
+	statements: Vec<Statement>
+}
+
+impl Program {
+	pub fn new() -> Self {
+		Program { statements: vec![] }
+	}
+}
+
+impl fmt::Display for Node {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Node::PROGRAM(p) => p.fmt(f),
+			Node::STATEMENT(s) => s.fmt(f),
+			Node::EXPRESSION(e) => e.fmt(f),
+		}
+	}
+}
+
+impl fmt::Display for Expression {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Expression::IDENT(i) => write!(f, "{}", i),
+			_ => write!(f, "handle me")
+		}
+	}
+}
+
+impl fmt::Display for Program {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		for curr in &self.statements {
+			write!(f, "{}", curr);
+		}
+		Ok(())
+	}
+}
+
+impl fmt::Display for Statement {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Statement::INCOMPLETE => write!(f, "INCOMPLETE"),
+			Statement::LET(s) => {
+				write!(f, "let {} = {};", s.name, s.value)
+			},
+			Statement::EXP(exp) => {
+				write!(f, "{}", exp)
+			}
+		}
+	}
+}
+
+//=== AST END ====
+
+//=== PARSER BEGIN ===
+
+#[test]
+fn test_parse_statement_let() {
+	let input = r#"
+let f = 03;
+let u = 07;
+let r = 15;
+let k = 32;
+let a = 34;
+let n = 35;"#;
+
+	let actual = Parser::new(Lexer::new(input.to_owned())).parse().unwrap();
+
+	let expecteds = vec![
+		Statement::LET(LetStatement{name: String::from("f"), value: Expression::IDENT(String::from("TODO"))}),
+		Statement::LET(LetStatement{name: String::from("u"), value: Expression::IDENT(String::from("TODO"))}),
+		Statement::LET(LetStatement{name: String::from("r"), value: Expression::IDENT(String::from("TODO"))}),
+		Statement::LET(LetStatement{name: String::from("k"), value: Expression::IDENT(String::from("TODO"))}),
+		Statement::LET(LetStatement{name: String::from("a"), value: Expression::IDENT(String::from("TODO"))}),
+		Statement::LET(LetStatement{name: String::from("n"), value: Expression::IDENT(String::from("TODO"))}),
+	];
+
+	assert_eq!(actual.statements.len(), 6);
+	assert_eq!(actual.statements, expecteds);
+
+
+
+}
+
+fn parse(input: &str) -> Program {
+	let l = Lexer::new(input.to_owned());
+
+	let mut p = Parser::new(l);
+
+	return p.parse().unwrap()
+}
+
+//pub type Result<T> = result::Result<T, ParserErrorType>;
+
+pub struct Parser {
+	lexer: Lexer,
+	curr_token: Token,
+	peek_token: Token,
+}
+
+#[derive(Debug)]
+pub enum ParserError {
+	TODO,
+	INVALID_TOKEN(Token),
+	UNEXPECTED_TOKEN{want: String, got: String},
+	NO_IDENT(Token),
+	UNEXPECTED_STATEMENT_TOKEN(Token),
+}
+
+impl fmt::Display for ParserError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			ParserError::TODO => write!(f, "todo"),
+			ParserError::INVALID_TOKEN(t) => write!(f, "invalid token: {}", t),
+			ParserError::UNEXPECTED_TOKEN { want, got } => write!(
+				f,
+				"parser found unexpected token: {}, expected: {}",
+				got, want,
+			),
+			ParserError::NO_IDENT(t) => write!(f, "no ident: {}", t),
+			ParserError::UNEXPECTED_STATEMENT_TOKEN(t) => write!(f, "unexpected statement token: {}", t),
+
+		}
+	}
+}
+
+impl Default for Parser {
+	fn default() -> Parser {
+		Parser {lexer: Lexer::default(), curr_token: Token::EOF, peek_token: Token::EOF}
+	}
+}
+
+impl Parser {
+	fn new(lexer: Lexer) -> Self {
+		let mut p = Parser::default();
+		p.lexer = lexer;
+
+		//call twice in order to set both of curr_token and peek_token
+		p.next_token();
+		p.next_token();
+
+		return p;
+	}
+
+	fn next_token(&mut self) -> Result<(), Error> {
+		self.curr_token = self.peek_token.clone();
+		self.peek_token = self.lexer.next_token();
+
+		Ok(())
+	}
+
+	fn curr_token_is(&self, token: Token) -> bool {
+		return self.curr_token == token
+	}
+
+	fn peek_token_is(&self, token: Token) -> bool {
+		return self.peek_token == token
+	}
+
+	fn expect_peek(&mut self, token: Token) -> Result<(), ParserError> {
+		if self.peek_token_is(token) {
+			self.next_token();
+			return Ok(());
+		}
+		return Err(ParserError::UNEXPECTED_TOKEN{want: format!("{}", self.peek_token), got: format!("{}", self.curr_token)})
+	}
+
+	//TODO: vec err arr
+	fn parse(&mut self) -> Result<Program, ParserError> {
+		let mut program = Program::new();
+		while self.curr_token != Token::EOF {
+			match self.parse_statement() {
+				Ok(s) => program.statements.push(s),
+				Err(e) => return Err(e)
+			}
+			self.next_token();
+		}
+		Ok(program)
+	}
+
+	fn parse_statement(&mut self) -> Result<Statement, ParserError> {
+		match self.curr_token {
+			Token::LET => self.parse_statement_let(),
+			_ => Ok(Statement::INCOMPLETE),
+			//_ => Err(ParserError::UNEXPECTED_STATEMENT_TOKEN(self.curr_token.clone())),
+		}
+	}
+
+	fn read_identifier(&mut self) -> Result<String, ParserError> {
+		match self.curr_token {
+			Token::IDENT(ref mut s) => Ok(s.clone()),
+			_ => Err(ParserError::NO_IDENT(self.curr_token.clone()))
+		}
+	}
+
+	fn parse_statement_let(&mut self) -> Result<Statement, ParserError> {
+		self.next_token();
+
+		let ident = self.read_identifier()?;
+
+		self.expect_peek(Token::ASSIGN)?;
+
+		//TODO: We're skipping the expressions until we encounter a semicolon
+		while !self.curr_token_is(Token::SEMICOLON) {
+			self.next_token();
+		}
+
+		Ok(Statement::LET(LetStatement{name: ident.clone(), value: Expression::IDENT(String::from("TODO"))}))
+	}
+}
+
+// === PARSER END ====
 
 #[test]
 fn test_next_token_basic() {
@@ -120,20 +395,16 @@ fn test_next_token_condition() {
 		Token::INT(15),
 		Token::RPAREN,
 		Token::LBRACE,
-
 		Token::RETURN,
 		Token::TRUE,
 		Token::SEMICOLON,
 		Token::RBRACE,
-
 		Token::ELSE,
-
 		Token::LBRACE,
 		Token::RETURN,
 		Token::FALSE,
 		Token::SEMICOLON,
 		Token::RBRACE,
-
 		Token::EOF,
 	];
 
@@ -179,7 +450,7 @@ impl Lexer {
 		} else if let Some(c) = self.input.chars().nth(self.read_pos) {
 			return c;
 		}
-		return '\0'
+		return '\0';
 	}
 
 	fn read_char(&mut self) {
@@ -231,7 +502,7 @@ impl Lexer {
 				} else {
 					t = Token::ASSIGN;
 				}
-			},
+			}
 			'!' => {
 				if self.peek_char() == '=' {
 					self.read_char();
@@ -239,7 +510,7 @@ impl Lexer {
 				} else {
 					t = Token::BANG;
 				}
-			},
+			}
 			_ => {
 				if Lexer::is_letter(self.ch) {
 					let id = self.read_identifier();
@@ -281,7 +552,7 @@ impl Lexer {
 	}
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
 	ILLEGAL(char),
 	EOF,
